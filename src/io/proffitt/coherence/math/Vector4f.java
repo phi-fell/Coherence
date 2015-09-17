64e79c3e3bf5705781956b7cd30e05ad01dcf988
nulltitleexception <clbprfftt@gmail.com>
2015-09-17 00:58:15 -0500

    started vector class

---
+++ src/io/proffitt/coherence/math/Vector4f.java
@@ -0,0 +1,21 @@
+package io.proffitt.coherence.math;
+
+public class Vector4f {
+	float x,y,z,w; // 3d vector in homogeneous coordinates
+	public Vector4f(){
+		x = 0;
+		y = 0;
+		z = 0;
+		w = 0;
+	}
+	public Vector4f(float a, float b, float c, float d){
+		x = a;
+		y = b;
+		z = c;
+		w = d;
+	}
+	public void 
+	public Vector4f cross(Vector4f rhs){
+		return new Vector4f();
+	}
+}
