da1bd6711945592d6a8379c53b5e987d428204fb
nulltitleexception <clbprfftt@gmail.com>
2015-09-18 01:02:19 -0500

    divideByW method

--- src/io/proffitt/coherence/math/Vector4f.java
+++ src/io/proffitt/coherence/math/Vector4f.java
@@ -1,21 +1,29 @@
 package io.proffitt.coherence.math;
 
 public class Vector4f {
-	float x,y,z,w; // 3d vector in homogeneous coordinates
-	public Vector4f(){
+	float	x, y, z, w; // 3d vector in homogeneous coordinates
+	public Vector4f() {
 		x = 0;
 		y = 0;
 		z = 0;
 		w = 0;
 	}
-	public Vector4f(float a, float b, float c, float d){
+	public Vector4f(float a, float b, float c, float d) {
 		x = a;
 		y = b;
 		z = c;
 		w = d;
 	}
-	public void 
-	public Vector4f cross(Vector4f rhs){
+	public void divideByW() {
+		if (w == 0) {
+			return;
+		}
+		x /= w;
+		y /= w;
+		z /= w;
+		w = 1;
+	}
+	public Vector4f cross(Vector4f rhs) {
 		return new Vector4f();
 	}
 }
